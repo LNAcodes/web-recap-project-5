e43a509a2c09b363cb9b9df5dae31d3c00ace73f
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-02-10 09:09:31 +0100

    Merge main changes

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,16 +2,22 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="navigation">
+    <nav className="navigation" aria-label="Main navigation">
       <ul className="navigation__list">
         <li className="navigation__list-item">
-          <Link href="/">Spotlight</Link>
+          <Link href="/" aria-label="Go to Spotlight page">
+            Spotlight
+          </Link>
         </li>
         <li className="navigation__list-item">
-          <Link href="/gallery/">Art Pieces</Link>
+          <Link href="/gallery/" aria-label="Go to Art Pieces gallery">
+            Art Pieces
+          </Link>
         </li>
         <li className="navigation__list-item">
-          <Link href="/favorites/">Favorites</Link>
+          <Link href="/favorites/" aria-label="Go to Favorites page">
+            Favorites
+          </Link>
         </li>
       </ul>
     </nav>
